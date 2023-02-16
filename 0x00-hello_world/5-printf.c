6eb346e6c9a983a8cf4f1e29e5a18eeca7bf9d84
Mercykemboi <kemboimercy49@gmail.com>
2023-02-16 05:23:47 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - where code is written
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
