f4db9c77d6d811e9b868d78dcc55a46d976d452e
Mercykemboi <kemboimercy49@gmail.com>
2023-02-22 03:40:12 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - code executes
+ * @n: caracter passed
+ * @b: character passed
+ * Return: aded value
+ */
+int add(int n, int b)
+{
+int n;
+int b;
+int x;
+x = n + b;
+_putchar('x');
+return (0);
+}
