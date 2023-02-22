80e6773d1f7e7f5dd14ad5a24e2e7c71f5f5701f
Mercykemboi <kemboimercy49@gmail.com>
2023-02-21 23:54:34 -0800

    5-sign.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,8 @@
 /**
  * _isalpha - checks the alphabetical letters
  * @c: character to be checked
- * Return : returns 0 and 1 depending on the condition
+ * Returns 'a','z','A','Z' if c is a letter, lowercase or uppercase
+ * Returns 0 otherwise
  */
 int _isalpha(int c)
 {
